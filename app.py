46f3b2a48434d9f3675ebbb4ed38ec68250481b7
paultrujillohormigo-pixel <paul.trujillo.hormigo@gmail.com>
2026-01-28 10:25:39 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 from decimal import Decimal
 from db import get_connection
+from costeo import costeo_bp
+
 import urllib.parse
 
 app = Flask(__name__)
